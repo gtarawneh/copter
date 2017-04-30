154c5fd47f985c40ad9eb976fea05c8d4defeab2
Ghaith Tarawneh <gtarawneh@gmail.com>
2017-04-30 22:51:28 +0100

    Refactor parse_sg

--- parse_sg.py
+++ parse_sg.py
@@ -1,18 +1,34 @@
 #!/usr/bin/env python
 
-from pprint import pprint
+from collections import namedtuple
 from itertools import product
 from itertools import combinations
+from itertools import permutations
+from itertools import starmap
 from bitarray import bitarray
+from pprint import pprint
 from json import dumps as jsons
+
+Transition = namedtuple("Transition", "signal polarity")
+Level = namedtuple("Level", "signal level")
+SG = namedtuple("SG", "transitions encoding")
+
+get_tran_str = lambda transition : transition.signal + transition.polarity
+get_level_str = lambda level : level.signal + level.level
+get_sg_tran = lambda sg, transition : sg.transitions[get_tran_str(transition)]
+
+def print_stg(sg):
+	print "Transitions:\n"
+	for key, val in sg.transitions.iteritems():
+		print "%4s : %s" % (key, val)
+	print "\nEncoding: %s\n" % sg.encoding
 
 def load_sg(file):
 	"""
 	Load an SG file.
-	Return a dictionary, mapping each transition to a list of (prev, next)
-	state tuples, for example: 'a+': [('000', '100'), ('010', '110')].
 	"""
-	sg = {}
+	# load transitions
+	transitions = {}
 	with open(file, "r") as fid:
 		lines = fid.read().splitlines()
 	for line in lines:
@@ -20,17 +36,11 @@ def load_sg(file):
 			prev_state, transition, next_state = line.split()
 			prev_bits = prev_state.split("_")[1]
 			next_bits = next_state.split("_")[1]
-			sg[transition] = sg.get(transition, [])
-			sg[transition].append((prev_bits, next_bits))
-	return sg
-
-def get_encoding(sg):
-	"""
-	Return the encoding of a state graph `sg` as a list of signals, for example
-	['a', 'b', 'c'].
-	"""
+			transitions[transition] = transitions.get(transition, [])
+			transitions[transition].append((prev_bits, next_bits))
+	# determine encoding
 	encoding_dic = {}
-	for transition, state_list in sg.iteritems():
+	for transition, state_list in transitions.iteritems():
 		signal, polarity = transition[:-1], transition[-1]
 		for state_tup in state_list:
 			prev_bits, next_bits = state_tup
@@ -50,102 +60,90 @@ def get_encoding(sg):
 	encoding_list = [None] * len(encoding_dic)
 	for key, value in encoding_dic.iteritems():
 		encoding_list[value] = key
-	return encoding_list
+	return SG(transitions, encoding_list)
 
-def get_rsvec(sg, nvars):
+def get_reachable_barr(sg):
 	"""
-	Calculate vector of reachable states.
+	Calculate bit array of reachable states.
 	"""
-	rstates = [trans[0] for trans_list in sg.values() \
+	nvars = len(sg.encoding)
+	rstates = [trans[0] for trans_list in sg.transitions.values() \
 		for trans in trans_list] # reachable states
-	rsvec = bitarray(2**nvars) # reachable state vector
-	rsvec.setall(0)
+	reachable_barr = bitarray(2**nvars) # reachable state bit array
+	reachable_barr.setall(0)
 	for item in rstates:
 		ind = int(item, 2)
-		rsvec[ind] = 1
-	return rsvec
+		reachable_barr[ind] = 1
+	return reachable_barr
 
-def get_tran_svec(sg, nvars, transition):
-	svec = bitarray(2**nvars) # reachable state vector
+def get_tran_barr(sg, transition):
+	nvars = len(sg.encoding)
+	svec = bitarray(2**nvars)
 	svec.setall(0)
-	for state, _ in sg[transition]:
+	for state, _ in get_sg_tran(sg, transition):
 		ind = int(state, 2)
 		svec[ind] = 1
 	return svec
 
-def get_literal_svec(nvars, ind):
+def get_level_barr(sg, level):
+	nvars = len(sg.encoding)
+	ind = sg.encoding.index(level.signal)
 	svec = bitarray([(i>>ind)&1 for i in range(2**nvars)])
-	return svec
+	return ~svec if level=="-" else svec
 
-def is_implication(preced, antec):
-	return antec & preced == antec
+def is_implication(cause, effect):
+	"""
+	Evaluate the statement (`cause` implies `effect`).
+
+	Return True iff cause is subset of effect.
+	"""
+	return cause & effect == cause
 
 def main():
 	file = "examples/david_cell.sg"
 	sg = load_sg(file)
-	encoding = get_encoding(sg)
-	nvars = len(encoding)
-	rsvec = get_rsvec(sg, nvars)
-	usvec = bitarray(rsvec)
-	usvec.invert()
-	# printing
-	pprint(sg)
-	print ""
-	print "Encoding :", encoding
-	print ""
+	signals = sg.encoding
+	reachable_barr = get_reachable_barr(sg)
+	print_stg(sg)
 	# mine atom causalities
 	primitive_concepts = []
-	rise_transitions = ["%s+" % signal for signal in encoding]
-	fall_transitions = ["%s-" % signal for signal in encoding]
-	all_transitions = rise_transitions + fall_transitions
-	for target_trans in all_transitions:
-		target = get_tran_svec(sg, nvars, target_trans)
-		for literal_ind in range(nvars):
-			literal_svec = get_literal_svec(5, literal_ind)
-			if is_implication(literal_svec, target):
-				literal = encoding[nvars-literal_ind-1]
-				prim = "cause %s+ %s" % (literal, target_trans)
+	psignals = list(product(signals, "+-"))
+	transitions = list(starmap(Transition, psignals))
+	levels = list(starmap(Level, psignals))
+	for tran in transitions:
+		tran_barr = get_tran_barr(sg, tran) & reachable_barr
+		for level in levels:
+			level_barr = get_level_barr(sg, level) & reachable_barr
+			if is_implication(tran_barr, level_barr):
+				prim = "cause %s %s" % (
+					get_level_str(level),
+					get_tran_str(tran)
+				)
 				primitive_concepts.append(prim)
-			if is_implication(~literal_svec, target):
-				literal = encoding[nvars-literal_ind-1]
-				prim = "cause %s- %s" % (literal, target_trans)
-				primitive_concepts.append(prim)
-	# mine OR causality (or_cause_rrr, or_case_fff)
-	or_cause_rrr_concepts = []
-	or_cause_fff_concepts = []
-	for comb in combinations(range(nvars), 2):
-		literal1_svec = get_literal_svec(5, comb[0])
-		literal2_svec = get_literal_svec(5, comb[1])
-		for target_signal in encoding:
-			target_rise = get_tran_svec(sg, nvars, "%s+" % target_signal) & rsvec
-			target_fall = get_tran_svec(sg, nvars, "%s-" % target_signal) & rsvec
-			comb_svec_or_rr = (literal1_svec | literal2_svec) & rsvec
-			comb_svec_or_rf = (~literal1_svec | ~literal2_svec) & rsvec
-			comb_svec_or_ff = (~literal1_svec | ~literal2_svec) & rsvec
-			if is_implication(comb_svec_or_rr, target_rise):
-				literal1 = encoding[nvars-comb[0]-1]
-				literal2 = encoding[nvars-comb[1]-1]
-				bogus = not comb_svec_or_rr.any()
-				oc_concept = "or_cause_rrr %s %s %s%s" % \
-					(literal1, literal2, target_signal, " (bogus)" if bogus else "")
-				or_cause_rrr_concepts.append(oc_concept)
-			if is_implication(comb_svec_or_rf, target_fall):
-				literal1 = encoding[nvars-comb[0]-1]
-				literal2 = encoding[nvars-comb[1]-1]
-				bogus = not comb_svec_or_rf.any()
-				if bogus:
-					print "BOGUS"
-				oc_concept = "or_cause_rfr %s %s %s%s" % \
-					(literal1, literal2, target_signal, " (bogus)" if bogus else "")
-				or_cause_rrr_concepts.append(oc_concept)
-			if is_implication(comb_svec_or_ff, target_fall):
-				literal1 = encoding[nvars-comb[0]-1]
-				literal2 = encoding[nvars-comb[1]-1]
-				bogus = not comb_svec_or_ff.any()
-				oc_concept = "or_cause_fff %s %s %s%s" % \
-					(literal1, literal2, target_signal, " (bogus)" if bogus else "")
-				or_cause_fff_concepts.append(oc_concept)
-	concepts = primitive_concepts + or_cause_fff_concepts + or_cause_rrr_concepts
+	# mine OR causality
+	signal_pairs = combinations(sg.encoding, 2)
+	pol_pairs = ["++", "+-", "--"]
+	or_combs = list(product(signal_pairs, pol_pairs))
+	get_comb_levels = lambda comb : \
+		(Level(comb[0][0], comb[1][0]), Level(comb[0][1], comb[1][1]))
+	level_tups = map(get_comb_levels, or_combs)
+	or_cause_concepts = []
+	for transition in transitions:
+		tran_barr = get_tran_barr(sg, transition) & reachable_barr
+		for tup in level_tups:
+			literal1_barr = get_level_barr(sg, tup[0])
+			literal2_barr = get_level_barr(sg, tup[1])
+			or_barr = (literal1_barr | literal2_barr) & reachable_barr
+			if is_implication(tran_barr, or_barr):
+				oc_concept = "or_cause %s %s %s" % (
+					get_level_str(tup[0]),
+					get_level_str(tup[1]),
+					get_tran_str(transition)
+				)
+				bogus = not or_barr.any()
+				oc_concept_b = "%s (bogus)" % concept if bogus else oc_concept
+				or_cause_concepts.append(oc_concept_b)
+	concepts = primitive_concepts + or_cause_concepts
 	print jsons(concepts, indent=4)
 
 if __name__ == "__main__":
