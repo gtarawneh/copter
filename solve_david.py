f1321f48a71e27a7d8c4220b694d0d0e2056627c
Ghaith Tarawneh <gtarawneh@gmail.com>
2017-05-05 18:44:38 +0300

    Add check for NOR concepts in solve_david

--- solve_david.py
+++ solve_david.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python
 
-from parse_sg import mine_concepts
-from json     import dumps as jsons
-from causality import *
+from parse_sg  import mine_concepts
+from json      import dumps as jsons
 from itertools import product
 from itertools import starmap
 from itertools import combinations
+from itertools import permutations
+from concepts  import *
 
 def main():
 	file = "examples/david_cell.sg"
 	sg, cause_concepts, or_cause_concepts = mine_concepts(file, False)
 	concepts = cause_concepts + or_cause_concepts
 	graph = dict()
-	signals = sg.encoding
-	literals = starmap(Literal, product(signals, "+-"))
+	all_signals = sg.encoding
+	literals = starmap(Literal, product(all_signals, "+-"))
 	# for cause in cause_concepts:
-	for cond, tran in combinations(literals, 2):
+	for cond, tran in permutations(literals, 2):
+		if is_negated(cond, tran):
+			continue
 		signals = set(sg.encoding)
 		signals.discard(cond.signal)
 		signals.discard(tran.signal)
@@ -23,9 +26,24 @@ def main():
 		mk_or_conc = lambda cond2 : OrCause(cond, cond2, tran)
 		cause = Cause(cond, tran)
 		graph[cause] = map(mk_or_conc, other_literals)
-	for key, val in graph.iteritems():
-		print "%-16s : %s" % (key, map(str, val))
+		print "%-16s %s" % (cause, map(str, graph[cause]))
 	# "orGate a b c = or_cause_rrr a b c . cause a- c- . cause b- c-"
+	# "norGate a b c = or_cause_rrf a b c . cause a- c+ . cause b- c+",
+	or_causes = set().union(*map(set, graph.values()))
+	for y in all_signals:
+		remaining = set(all_signals) - set([y])
+		for a, b in combinations(remaining, 2):
+			or_cause = OrCause(
+				Literal(a, "+"),
+				Literal(b, "+"),
+				Literal(y, "-")
+			)
+			nor_gate = NorGate(a, b, y)
+			cause1 = Cause(Literal(a, "-"), Literal(y, "+"))
+			cause2 = Cause(Literal(b, "-"), Literal(y, "+"))
+			if (or_cause in or_cause_concepts) and (cause1 in cause_concepts) \
+				and (cause2 in cause_concepts):
+				print "%-16s = %s . %s . %s" % (nor_gate, or_cause, cause1, cause2)
 
 if __name__ == "__main__":
 	main()
