69f764c86d2a46a68be6488d5d82072871637e3e
Ghaith Tarawneh <gtarawneh@gmail.com>
2017-05-05 18:01:45 +0300

    Rename 'causality.py' to 'concepts.py'
    Also add OrGate and NorGate concepts

---
+++ concepts.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+from collections import namedtuple
+
+class Literal(namedtuple("Literal", "signal polarity")):
+
+	def __str__(self):
+		return self.signal + self.polarity
+
+class Cause(namedtuple("Cause", "cond transition")):
+
+	def __str__(self):
+		return "cause %s %s" % (self.cond, self.transition)
+
+class OrCause(namedtuple("OrCause", "cond1 cond2 transition")):
+
+	def __get_ordered(self):
+		return (self.cond1, self.cond2) if (self.cond1 < self.cond2) \
+			else (self.cond2, self.cond1)
+
+	def __str__(self):
+		return "or_cause %s %s %s" % (self.cond1, self.cond2, self.transition)
+
+	def __eq__(self, x):
+		tran_match = self.transition == x.transition
+		cond_match = self.__get_ordered() == x.__get_ordered()
+		return tran_match and cond_match
+
+class OrGate(namedtuple("OrGate", "a b y")):
+
+	def __get_ordered(self):
+		return (self.a, self.b) if (self.a < self.b) else (self.b, self.a)
+
+	def __str__(self):
+		ordered = self.__get_ordered()
+		return "orGate %s %s %s" % (ordered[0], ordered[1], self.y)
+
+	def __eq__(self, other):
+		output_match = self.y == other.y
+		input_match = self.__get_ordered() == other.__get_ordered()
+		return output_match and input_match
+
+	def __hash__(self):
+		ordered = __get_ordered(self)
+		return hash((ordered, self.y))
+
+class NorGate(namedtuple("NorGate", "a b y")):
+
+	def __get_ordered(self):
+		return (self.a, self.b) if (self.a < self.b) else (self.b, self.a)
+
+	def __str__(self):
+		ordered = self.__get_ordered()
+		return "norGate %s %s %s" % (ordered[0], ordered[1], self.y)
+
+	def __eq__(self, other):
+		output_match = self.y == other.y
+		input_match = self.__get_ordered() == other.__get_ordered()
+		return output_match and input_match
+
+	def __hash__(self):
+		ordered = __get_ordered(self)
+		return hash((ordered, self.y))
+
+def is_negated(x, y):
+	return (x.signal == y.signal) and (x.polarity != y.polarity)
