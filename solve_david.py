86dc65b53f371eeb9f1c18155f16da730cf75e1e
Ghaith Tarawneh <gtarawneh@gmail.com>
2017-05-17 18:02:09 +0100

    Fix issues in solve_david.py

--- solve_david.py
+++ solve_david.py
@@ -14,38 +14,6 @@ def main():
 	file = "examples/david_cell.sg"
 	sg, cause_concepts, or_cause_concepts = mine_concepts(file, False)
 	concepts = cause_concepts + or_cause_concepts
-	graph = dict()
-	all_signals = sg.encoding
-	literals = starmap(Literal, product(all_signals, "+-"))
-	# for cause in cause_concepts:
-	for cond, tran in permutations(literals, 2):
-		if cond == ~tran:
-			continue
-		signals = set(sg.encoding)
-		signals.discard(cond.signal)
-		signals.discard(tran.signal)
-		other_literals = starmap(Literal, product(signals, "+-"))
-		mk_or_conc = lambda cond2 : OrCause(cond, cond2, tran)
-		cause = Cause(cond, tran)
-		graph[cause] = map(mk_or_conc, other_literals)
-	# "orGate a b c = or_cause_rrr a b c . cause a- c- . cause b- c-"
-	# "norGate a b c = or_cause_rrf a b c . cause a- c+ . cause b- c+",
-	or_causes = set().union(*map(set, graph.values()))
-	for y in all_signals:
-		remaining = set(all_signals) - set([y])
-		for a, b in combinations(remaining, 2):
-			or_cause = OrCause(
-				Literal(a, "+"),
-				Literal(b, "+"),
-				Literal(y, "-")
-			)
-			nor_gate = NorGate(a, b, y)
-			cause1 = Cause(Literal(a, "-"), Literal(y, "+"))
-			cause2 = Cause(Literal(b, "-"), Literal(y, "+"))
-			if (or_cause in or_cause_concepts) and \
-				(cause1 in cause_concepts) and \
-				(cause2 in cause_concepts):
-				graph[nor_gate] = [or_cause, cause1, cause2]
 	costs = defaultdict(lambda : 1)
 	spec = [
 		OrCause(
@@ -62,6 +30,40 @@ def main():
 			Literal("a", "+")
 		)
 	]
+	spec = concepts
+	graph = dict()
+	all_signals = sg.encoding
+	literals = starmap(Literal, product(all_signals, "+-"))
+	# for cause in cause_concepts:
+	for cond, tran in permutations(literals, 2):
+		if cond == ~tran:
+			continue
+		signals = set(sg.encoding)
+		signals.discard(cond.signal)
+		signals.discard(tran.signal)
+		other_literals = starmap(Literal, product(signals, "+-"))
+		mk_or_conc = lambda cond2 : OrCause(cond, cond2, tran)
+		cause = Cause(cond, tran)
+		cause_children = map(mk_or_conc, other_literals)
+		if all([x in spec for x in cause_children]) or cause in spec:
+			graph[cause] = cause_children
+	# "orGate a b c = or_cause_rrr a b c . cause a- c- . cause b- c-"
+	# "norGate a b c = or_cause_rrf a b c . cause a- c+ . cause b- c+",
+	or_causes = set().union(*map(set, graph.values()))
+	for y in all_signals:
+		remaining = set(all_signals) - set([y])
+		for a, b in combinations(remaining, 2):
+			or_cause = OrCause(
+				Literal(a, "+"),
+				Literal(b, "+"),
+				Literal(y, "-")
+			)
+			nor_gate = NorGate(a, b, y)
+			cause1 = Cause(Literal(a, "-"), Literal(y, "+"))
+			cause2 = Cause(Literal(b, "-"), Literal(y, "+"))
+			nor_children = [or_cause, cause1, cause2]
+			if all([x in spec for x in nor_children]) or nor_gate in spec:
+				graph[nor_gate] = nor_children
 	solve(graph, costs, spec)
 
 if __name__ == "__main__":
